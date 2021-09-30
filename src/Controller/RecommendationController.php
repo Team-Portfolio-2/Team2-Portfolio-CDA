f32968ce866969402d0b1ce0b1480436e81e2c8d
Jean Paul <hotonk@outlook.com>
2021-09-30 19:19:01 +0200

    Ajoute Controllers

---
+++ src/Controller/RecommendationController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Dao\PortefolioDao;
+use App\Model\PortefolioModele;
+use PDOException;
+
+class RecommendationController
+{
+
+
+    public function addc(): void
+    {
+
+    }
+    
+    public function editRecommendation(int $id_task): void
+    {
+
+    }
+
+
+
+    public function deleteRecommendation(int $id_task): void
+    {
+
+    }
+
+}
---
+++ src/Controller/TaskController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Dao\PortefolioDao;
+use App\Model\PortefolioModele;
+use PDOException;
+
+class TaskController
+{
+
+
+    public function addTask(): void
+    {
+
+    }
+    
+    public function editTask(int $id_task): void
+    {
+
+    }
+
+
+
+    public function deleteTask(int $id_task): void
+    {
+
+    }
+
+}
