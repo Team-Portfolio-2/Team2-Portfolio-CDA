1a2de5532f41652c1f0c315cf8fc360c9a9af09f
Michel Guinis <michelguinis@gmail.com>
2021-10-01 10:32:23 +0200

    views page update

--- views/admin/educations.html.php
+++ views/admin/educations.html.php
@@ -1,4 +1,11 @@
 <?php
-
-
+foreach ($eductions as $education) : ?>
+    <article>
+        <h2><?= $education->getName() ?></h2>
+        <span><?= $education->getCompany() ?></span>
+        <p><?= nl2br($education->getDescription()) ?></p>
+        <a href="<?= sprintf('/project/%d/show', $education->getId()) ?>">Voir le détail du projet</a>
+    </article>
+<?php
+endforeach;
 ?>
