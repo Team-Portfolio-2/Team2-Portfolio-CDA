f3d7da3fc5e0ba7377b596f44a9da072756b31e9
Michel Guinis <michelguinis@gmail.com>
2021-10-01 15:54:56 +0200

    comit modif

--- views/admin/project.html.php
+++ views/admin/project.html.php
@@ -1,1 +1,18 @@
 <?php
+<<<<<<< Updated upstream
+=======
+require TEMPLATES . DIRECTORY_SEPARATOR . "header.html.php";
+?>
+
+<article>
+    <h2><?= $project->getTitle() ?></h2>
+    <span><?= $project->getCreatedAt() ?></span>
+    <p><?= nl2br($project->getContent()) ?></p>
+    <a href="<?= sprintf('/admin/project/%d/edit', $project->getId()) ?>">Editer l'article</a>
+    <a href="<?= sprintf('/admin/project/%d/delete', $article->getId()) ?>">Supprimer l'article</a>
+</article>
+
+<?php
+require TEMPLATES . DIRECTORY_SEPARATOR . "footer.html.php";
+?>
+>>>>>>> Stashed changes
