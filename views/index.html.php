9fcf186ed090ebe7ad69ec878e2e6dc953a686ea
Michel Guinis <michelguinis@gmail.com>
2021-10-01 09:00:39 +0200

    base code in index and projects

--- views/index.html.php
+++ views/index.html.php
@@ -1,16 +1,10 @@
 <?php
 require TEMPLATES . DIRECTORY_SEPARATOR . "header.html.php";
 ?>
-<h1>Liste des articles</h1>
-<?php foreach ($articles as $article) : ?>
-    <article>
-        <h2><?= $article->getTitle() ?></h2>
-        <span><?= $article->getCreatedAt() ?></span>
-        <p><?= nl2br($article->getContent()) ?></p>
-        <a href="<?= sprintf('/article/%d/show', $article->getId()) ?>">Voir l'article</a>
-    </article>
-<?php
-endforeach;
-
+<h1>Mes Projets</h1>
+<?php 
+require TEMPLATES . DIRECTORY_SEPARATOR . "/admin/projects.html.php";
+?>
+<?php 
 require TEMPLATES . DIRECTORY_SEPARATOR . "footer.html.php";
 ?>
