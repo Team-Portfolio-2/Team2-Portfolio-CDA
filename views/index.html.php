5612c30563085b8b5df0c7840d9318d715292763
Michel Guinis <michelguinis@gmail.com>
2021-10-01 11:31:39 +0200

    Update index.html.php
    home page mod

--- views/index.html.php
+++ views/index.html.php
@@ -5,6 +5,13 @@ require TEMPLATES . DIRECTORY_SEPARATOR . "header.html.php";
 <?php 
 require TEMPLATES . DIRECTORY_SEPARATOR . "/admin/projects.html.php";
 ?>
+
+<h1>Mon Education</h1>
+<?php 
+require TEMPLATES . DIRECTORY_SEPARATOR . "/admin/educations.html.php";
+?>
+
+
 <?php 
 require TEMPLATES . DIRECTORY_SEPARATOR . "footer.html.php";
 ?>
