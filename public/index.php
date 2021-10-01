0dea2a44c1eb5273d3b3224ebb0f19575d7424be
Michel Guinis <michelguinis@gmail.com>
2021-10-01 11:50:50 +0200

    updated views with 404

--- public/index.php
+++ public/index.php
@@ -9,15 +9,19 @@ $router = Router::getInstance();
 
 require implode(DIRECTORY_SEPARATOR, [ROOT, 'config', 'routes.php']);
 
+dump($router);
+
 // Appliquer l'action
 try {
     $route = $router->match();
+    
     // (new App\Controller\ArticleController())->index()
     // (new App\Controller\ArticleController())->show($id)
     (new ($route->getController())())->{$route->getAction()}(...$router->getMatches());
     
 } 
 catch (RouteNotFoundException $e) {
+    dump($e);
     echo $e->getMessage();
     // header("Location: /"); // ou error 404
     // exit;
